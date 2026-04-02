df690e6eeb04c42890f2531f5a4c63981e5820a3
Sahil Siddiquie <siddiquie693@gmail.com>
2026-04-02 16:52:28 +0530

    file added

---
+++ server.py
@@ -0,0 +1,97 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+
+class SimpleAPI(BaseHTTPRequestHandler):
+      
+      # ----SimpleAPI-----
+      # A minimal REST API implemented using python built-in HTTP server.
+      # this class handles HTTP GET and POST request for managing users.
+      #-----Endpoint------
+      # GET /users - > returns list of users
+      # POST /users -> creates a new user
+
+    # Initialize with some default users
+      users = ["Alice", "Bob"]
+
+      def send_json(self, data, status=200):
+        """
+        send a JSON response to the client.
+
+        Args:
+        data(dict): python dictionary to be converted to JSON and sent as response
+        status(int): HTTP status code for the response (default: 200)
+        """
+
+        # write http status code line (e.g HTTP/1.1 200 OK)
+        self.send_response(status)
+
+        #inform the client that the response is in JSON format
+        self.send_header("Content-Type", "application/json")
+
+        # Signal end of the http headers
+        self.end_headers()
+
+        # convert python dict -> JSON -> bytes and write to response stream
+        self.wfile.write(json.dumps(data).encode())
+
+      def do_GET(self):
+        """
+        Handle HTTP GET requests.
+        Endpoint: /users - returns list of users
+        """
+        # route matching: check request url path
+        if self.path == "/users":
+
+            # send list of user with 200 OK status
+            self.send_json({"users": self.users})
+    
+      def do_POST(self):
+        """
+        Handle HTTP POST requests:
+        Endpoint: /users - creates a new user
+        """
+        if self.path == "/users":
+
+            # Read content-length header to know body size
+            content_length = int(self.headers.get("Content-Length", 0))
+
+            # Read raw request body from socket(bytes)
+            body = self.rfile.read(content_length)
+
+            try:
+                data = json.loads(body)
+            except json.JSONDecodeError:
+                self.send_json({"error": "Invalid JSON"}, status=400)
+                return
+            
+            if "name" not in data:
+                # Return 400 if 'name' is missing
+                self.send_json({"error": "Name is required"}, 400)
+                return
+            
+            # Add new user to the list and return updated list with 201 Created status
+            self.users.append(data["name"])
+            self.send_json(
+                {"message": "User created", "users": self.users}, 201
+            )  
+# Create and start the server instance
+server = HTTPServer(('localhost', 8000), SimpleAPI)
+
+#Inform user that server has started
+print("Server running at http://localhost:8000")
+
+# Start the server to handle requests indefinitely until interrupted
+server.serve_forever()                
+
+
+# Testing in postman
+# GET http://localhost:8000/users
+# Response: {"users": ["Alice", "Bob"]}
+
+# POST http://localhost:8000/users
+# Body: {"name": "Charlie"}
+# Response: {"message": "User created", "users": ["Alice", "Bob", "Charlie"]}
+
+# verify user was added
+# GET http://localhost:8000/users
+# Response: {"users": ["Alice", "Bob", "Charlie"]}
